fc0794a0f86150a8ca124fd77a34b9455577e87c
Omkar Chakradhar Thawakar <omkarchakradharthawakar@omkars-macbook-air.local>
2017-03-24 22:10:47 +0530

    Initial Commit

---
+++ omeefirst/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  omeefirst
+//
+//  Created by Omkar Chakradhar Thawakar on 24/03/17.
+//  Copyright © 2017 Omkar Chakradhar Thawakar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
